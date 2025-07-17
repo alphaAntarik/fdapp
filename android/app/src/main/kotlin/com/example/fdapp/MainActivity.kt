461be7285aebabef0705f50f9276ae237505af04
alphaAntarik <antarikpanditison@gmail.com>
2025-07-17 10:58:09 +0530

    abc

--- android/app/src/main/kotlin/com/example/fdapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fdapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- migrations/1_migration.js
+++
@@ -1,5 +0,0 @@
-const UserContract = artifacts.require("UsersContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(UserContract);
-};
